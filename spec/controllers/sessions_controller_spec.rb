00db6613a032f80a5f90b743bafe0186864101b8
Chris Gilbert <chrisgilbert4@icloud.com>
2019-09-07 18:23:34 +0100

    Add sessions controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
 
   describe "GET #new" do
     it "returns http success" do
@@ -9,11 +10,41 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
+  describe 'POST #create' do
+    context 'with an existing user and valid params' do
+      it 'it authenticates the user and logs them in' do
+        expect(subject).to receive(:log_in).with(user)
+        post :create, params: { session: { email: user.email, password: user.password } }
+        expect(response).to redirect_to dashboard_path
+      end
+    end
+
+    context 'with invalid params' do
+      it 're-renders the `new` form' do
+        post :create, params: { session: { email: user.email, password: nil } }
+        expect(response).to render_template "new"
+      end
+    end
+
+    context 'without an existing user' do
+      it 're-renders the `new` form' do
+        post :create, params: { session: { email: nil, password: nil } }
+        expect(response).to render_template "new"
+      end
+    end
+  end
+
   describe "GET #destroy" do
+    context 'when a user is logged in' do
+      it 'logs the user out' do
+        allow(subject).to receive(:logged_in?).and_return(true)
+        expect(subject).to receive(:log_out)
+        get :destroy
+      end
+    end
     it "returns http success" do
       get :destroy
       expect(response).to redirect_to login_path
     end
   end
-
 end
