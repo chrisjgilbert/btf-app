148c0e65608f804ddb9d6579538c27a992af6ab6
Chris Gilbert <chrisgilbert4@icloud.com>
2019-09-15 13:14:10 +0100

    Fix type in schema

--- db/migrate/20190915093316_create_competitions.rb
+++ db/migrate/20190915093316_create_competitions.rb
@@ -1,4 +1,4 @@
-class CreateCompetition < ActiveRecord::Migration[5.2]
+class CreateCompetitions < ActiveRecord::Migration[5.2]
   def change
     create_table :competitions do |t|
       t.string :name
