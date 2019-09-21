e5ef957368d86c1cae5a5da736f6d50d0e722005
Chris Gilbert <chrisgilbert4@icloud.com>
2019-09-21 15:38:31 +0100

    Remove captain stuff

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,6 @@ class Team < ApplicationRecord
   has_many :picks, -> { order(created_at: :asc) }
   has_many :competitors, through: :picks
   accepts_nested_attributes_for :picks
-  has_one :captain, class_name: "Competitor"
 
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
