04a90cd0bf11cdcad47e3ddd0b87cda15dabcb7f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-09-21 17:00:28 +0100

    Add feature test for team pick

--- spec/features/create_and_pick_team_spec.rb
+++ spec/features/create_and_pick_team_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Create team", type: :feature do
   let(:user) { FactoryBot.build(:user) }
+  fixtures :competitions
+  fixtures :competitors
 
   scenario "with valid credentials" do
     valid_sign_up(user)
@@ -10,9 +12,16 @@ RSpec.feature "Create team", type: :feature do
     click_link "click here"
     
     fill_in :team_name, with: 'New Team'
+
+    select "England", :from => "team_picks_attributes_0_competitor_id"
+    select "Steve Smith", :from => "team_picks_attributes_1_competitor_id"
+    select "Harry Kane", :from => "team_picks_attributes_2_competitor_id"
     click_button 'Submit'
 
     expect(page).to have_selector('h1', text: 'New Team')
+    expect(page).to have_selector('li', text: 'England for Rugby World Cup')
+    expect(page).to have_selector('li', text: 'Steve Smith for Ashes')
+    expect(page).to have_selector('li', text: 'Harry Kane for Golden Boot')
   end
 
   scenario "with invalid credentials" do
