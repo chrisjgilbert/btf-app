7c6f116e6a69d38fd9beb6aae2cd8048204d5a1d
Chris Gilbert <chrisgilbert4@icloud.com>
2019-10-05 12:48:04 +0100

    Create league funcionality

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -4,13 +4,32 @@ class LeaguesController < ApplicationController
   end
 
   def new
+    @league = League.new
   end
 
   def create
+    @league = League.new(league_params)
+    @league.user = current_user
+    if @league.save
+      add_team_to_league(@league, current_user.team)
+      flash[:success] = 'League successfully created!'
+      redirect_to @league
+    else
+      render 'new'
+    end
   end
-
 
   def show
     @league = League.find(params[:id])
+  end
+
+  private
+
+  def league_params
+    params.require(:league).permit(:name)
+  end
+
+  def add_team_to_league(league, team)
+    LeagueMembership.create(league_id: league.id, team_id: team.id)
   end
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -2,4 +2,6 @@ class League < ApplicationRecord
   belongs_to :user, optional: true
   has_many :teams, through: :league_memberships
   has_many :league_memberships
+
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
 end
