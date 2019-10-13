5d1d3f4b3b4f0fd111e84fb492ac77ead29b4649
Chris Gilbert <chrisgilbert4@icloud.com>
2019-10-13 11:38:34 +0100

    Captain selection complete

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -49,14 +49,13 @@ CaptainSelect.prototype.getCurrentPickIds = function() {
 
 CaptainSelect.prototype.updateOptions = function() {
   var prevPickIds = this.pickIds;
-  pickIds = this.getCurrentPickIds()
+  this.pickIds = this.getCurrentPickIds()
   this.currentCaptainId = this.getCurrentCaptainId();
-  // if user replaces their current captain selecion, replace with replacement selection
-  if (!pickIds.includes(this.currentCaptainId) && prevPickIds.length > 0) {
+  if (!this.pickIds.includes(this.currentCaptainId) && prevPickIds !== undefined) {
     prevCaptainIndex = prevPickIds.indexOf(this.currentCaptainId)
-    this.currentCaptainId = pickIds[prevCaptainIndex]
+    this.currentCaptainId = this.pickIds[prevCaptainIndex]
   }
-  this.postData(pickIds, this.currentCaptainId);
+  this.postData(this.pickIds, this.currentCaptainId);
 },
 
 CaptainSelect.prototype.postData = function(pickIds, currentCaptainId) {
@@ -67,71 +66,3 @@ CaptainSelect.prototype.postData = function(pickIds, currentCaptainId) {
   });
   return true;
 }
-
-  // var competitons = $('select') // we don't want to select the captain dropdown
-  // var pickIds = []
-  // var currentCaptainId;
-
-  // function initialize() {
-  //   setInitialOptions();
-  //   listenForSelectionChanges();
-  //   listendForCurrentCaptainSelection();
-  // };
-
-  // function setInitialOptions() {
-  //   pickIds = getCurrentPickIds();
-  //   currentCaptainId = '<%= @current_captain.id %>';
-  //   postData(pickIds, currentCaptainId);
-  // };
-
-  // function listenForSelectionChanges() {
-  //   competitons.each(function() {
-  //     $(this).change(function() {
-  //       updateOptions();
-  //     });
-  //   });
-  // };
-
-  // function listendForCurrentCaptainSelection() {
-  //   $('#team_captain_id').change(function() {
-  //     currentCaptainId = getCurrentCaptainId();
-  //     pickIds = getCurrentPickIds();
-  //     postData(pickIds, currentCaptainId);
-  //   });
-  // }
-
-  // function getCurrentCaptainId() {
-  //   return $('#team_captain_id :selected').val();
-  // }
-
-  // function getCurrentPickIds() {
-  //   pickIds = []
-  //   competitons.each(function() {
-  //     pickIds.push($(this).val())
-  //   });
-  //   return pickIds;
-  // };
-
-  // function updateOptions() {
-  //   var prevPickIds = pickIds;
-  //   pickIds = getCurrentPickIds()
-  //   currentCaptainId = getCurrentCaptainId();
-  //   // if user replaces their current captain selecion, replace with replacement selection
-  //   if (!pickIds.includes(currentCaptainId) && prevPickIds.length > 0) {
-  //     prevCaptainIndex = prevPickIds.indexOf(currentCaptainId)
-  //     currentCaptainId = pickIds[prevCaptainIndex]
-  //   }
-  //   postData(pickIds, currentCaptainId);
-  // };
-
-  // function postData(pickIds, currentCaptainId) {
-  //   console.log(pickIds, currentCaptainId)
-  //   $.post("/set_captain_options", {data: {captainOptions : pickIds, currentCaptainId : currentCaptainId}}, function(data, status) {
-  //     if (status != "success") {
-  //       alert('Woah, we couldn"t update the captain choices. Please try again or contact us if the problem persists.');
-  //     }
-  //   });
-  //   return true;
-  // };
-
-  // initialize();
