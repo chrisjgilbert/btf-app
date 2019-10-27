8f81f4ae5fe5563fca381b043540d347d100e343
Chris Gilbert <chrisgilbert4@icloud.com>
2019-10-27 14:04:46 +0000

    Add User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-  attr_accessor :password_reset_token
+  attr_accessor :password_reset_token, :activation_token
 
   has_one :team, dependent: :destroy
   has_one :league, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
-  before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 255 }
@@ -58,5 +59,16 @@ class User < ApplicationRecord
 
   def owns_league?(league_id)
     League.where(id: league_id, user_id: self.id).exists?
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_digest)
+  end
+
+  def downcase_email
+    email.downcase!
   end
 end
