515d767eec741c7d314fb9387dfb380c256fe47b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-11-03 16:37:50 +0000

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,18 @@ Dir.glob("#{Rails.root}/db/seeds/*.csv").map do |file|
   competition = nil
   favourite = nil
   competitor = nil
+  start_date = nil
+  end_date = nil
   competitor_count = 0
 
   csv_text = File.read(file)
   csv = CSV.parse(csv_text, :headers => false)
   csv.each.with_index do |row, index|
-
+    # competition
     if index == 0
-      puts "****ADDING NEW COMPETITON****"
+      puts "************************************"
+      puts "******Creating new competition******"
+      puts "************************************"
       puts "Creating #{row}"
       competition = Competition.new(name: row.join)
 
@@ -20,24 +24,38 @@ Dir.glob("#{Rails.root}/db/seeds/*.csv").map do |file|
       else
         puts "Failed to create #{competition.name}"
       end
-
+    # start date
     elsif index == 1
-      puts "Adding #{row} to #{competition.name}"
+      date = row.join.split(",")
+      start_date = Date.new(date[0].to_i, date[1].to_i, date[2].to_i)
+      if competition.update(start_date: start_date)
+        puts "Added start date of #{start_date} to #{competition.name}"
+      else
+        puts "Failed to add a start date to #{competition.name}"
+      end
+    # end date
+    elsif index == 2
+      date = row.join.split(",")
+      end_date = Date.new(date[0].to_i, date[1].to_i, date[2].to_i)
+      if competition.update(end_date: end_date)
+        puts "Added end date of #{end_date} to #{competition.name}"
+      else
+        puts "Failed to add a end date to #{competition.name}"
+      end
+    # favourite 
+    elsif index == 3
       favourite = Competitor.new(name: row.join, competition_id: competition.id)
-
       if favourite.save
         puts "Created #{favourite.name} and added to #{competition.name}"
         competitor_count += 1
       else
         puts "Failed to create #{favourite.name}"
       end
-
+    # competitors
     else
-      puts "Adding #{row} to #{competition.name}"
       competitor = Competitor.new(name: row.join, competition_id: competition.id)
-
       if competitor.save
-        puts "Created #{competitor.name} to #{competition.name}"
+        puts "Created #{competitor.name} and added to #{competition.name}"
         competitor_count += 1
       else
         puts "Failed to create #{competitor.name}"
@@ -46,18 +64,19 @@ Dir.glob("#{Rails.root}/db/seeds/*.csv").map do |file|
   end
 
   if competition.update(favourite_id: favourite.id)
-    puts "Added #{favourite.name} to #{competition.name}"
+    puts "Set #{favourite.name} as #{competition.name} favourite"
   else
     puts "Failed to add #{favourite.name} to #{competition.name}"
   end
 
   puts "************************"
-  puts "Created #{competition.name}..."
+  puts "Created #{competition.name} with a start date of #{competition.start_date} and end date of #{competition.end_date}..."
   puts "The favourite is #{favourite.name}"
   puts "Added #{competitor_count} competitors...."
   puts "************************"
 end
 
+# League creation
 league = League.new(name: 'The BTF Main League')
 if league.save
   puts "Created #{league.name}"
@@ -65,4 +84,6 @@ else
   puts "Failed to create #{league.name}"
 end
 
-puts "Seeding complete"
+puts "************************"
+puts "****Seeding complete****"
+puts "************************"
