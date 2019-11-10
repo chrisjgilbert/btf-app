4c21fcc737f8aa996bc33d5c5cf5289b38116f9b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-11-10 17:05:02 +0000

    Fix typo on form

--- app/models/team_validator.rb
+++ app/models/team_validator.rb
@@ -1,5 +1,5 @@
 class TeamValidator < ActiveModel::Validator
-  FAVOURITE_LIMIT = 3
+  FAVOURITE_LIMIT = 5
   def validate(record)
     if record.picks.select { |pick| pick.competitor.is_favourite? }.count > FAVOURITE_LIMIT
       record.errors[:base] << "You can't choose more than #{FAVOURITE_LIMIT} favourites"
