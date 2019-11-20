6fc0726700994e5d23046bf6aa9d30fd114e5185
Chris Gilbert <chrisgilbert4@icloud.com>
2019-11-20 21:07:17 +0000

    Tidy uo

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,13 +2,9 @@ class Competition < ApplicationRecord
   default_scope { order('competitions.start_date') }
   has_many :competitors
 
-  # def self.order_by_start_date(index) # TODO: check if this is actually doing anything
-  #   order('competitions.start_date').take(index).last
-  # end
-
   def self.favourites
-    all.map(&:favourite_id)
-    # [21, 512, 1846, 170, 502, 916, 1852, 812, 662, 1166, 9, 1877, 1784, 2001, 861, 1816, 926, 202, 1, 1466, 832, 1484, 2027, 146, 1804]
+    # all.map(&:favourite_id)
+    [21, 512, 1846, 170, 502, 916, 1852, 812, 662, 1166, 9, 1877, 1784, 2001, 861, 1816, 926, 202, 1, 1466, 832, 1484, 146, 1804]
   end
 
   def favourite
