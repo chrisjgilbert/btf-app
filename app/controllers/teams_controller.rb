6eb18fa7e6aa005a43954a156de356ebff008144
Chris Gilbert <chrisgilbert4@icloud.com>
2019-11-24 21:09:51 +0000

    Fix bug where if you picked 0 picks form would error

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -22,7 +22,9 @@ class TeamsController < ApplicationController
       create_team_success_flash_message
       redirect_to @team
     else
-      @current_captain = Competitor.find(team_params[:captain_id])
+      if team_params[:captain_id].present?
+        @current_captain = Competitor.find(team_params[:captain_id])
+      end
       render 'new'
     end
   end
