46e19294ed98920ae5c6c5b8d133e0f4fb2cbc94
Chris Gilbert <chrisgilbert4@icloud.com>
2019-11-29 19:41:17 +0000

    Udate flash messages and account activation copy

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,12 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
 
-    mail to: user.email, subjexct: 'Account activation'
+    mail to: user.email, subject: 'BTF Account Activation'
   end
 
   def password_reset(user)
     @user = user
 
-    mail to: user.email, subject: 'Password reset'
+    mail to: user.email, subject: 'BTF account password reset'
   end
 end
