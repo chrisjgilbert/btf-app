4a627a6f928b5343d9ae4c8b1558c68b3a9ff511
Chris Gilbert <chrisgilbert4@icloud.com>
2019-11-30 09:58:29 +0000

    Centre align flash text and show teams competing count in league show

--- app/helpers/league_memberships_helper.rb
+++ app/helpers/league_memberships_helper.rb
@@ -1,6 +1,6 @@
 module LeagueMembershipsHelper
   def successful_league_membership_create_flash_msg(league_name)
-    flash[:info] = "You have joined #{league_name}!"
+    # flash[:info] = "You have joined #{league_name}!"
   end
 
   def unsuccessful_league_membership_create_flash_msg
@@ -12,6 +12,6 @@ module LeagueMembershipsHelper
   end
 
   def unsuccessful_league_membership_destroy_flash_msg
-    flash[:danger] = 'Woah, something went wrong. We couldn"t remove you from that league. Please notify us and we will look into it!'
+    # flash[:danger] = 'Woah, something went wrong. We couldn"t remove you from that league. Please notify us and we will look into it!'
   end
 end
