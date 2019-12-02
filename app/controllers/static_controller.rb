d95e452f3fdb8b17802f99b644f0bcc38aa95a5a
Chris Gilbert <chrisgilbert4@icloud.com>
2019-12-02 07:49:25 +0000

    Add payment link to rules and guidance

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   before_action :logged_in_user, except: [:root]
   def rules_and_guidance
+    @payment_link = 'https://monzo.me/danielwilliams502/20.00?d=BTF%202020%20(type%20email%20address%20here)%20'.html_safe
   end
 
   def blog
