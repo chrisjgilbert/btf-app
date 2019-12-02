2fbb767396552443559baf3b873a9b1abb6c57a7
Chris Gilbert <chrisgilbert4@icloud.com>
2019-12-02 21:32:20 +0000

    Remove logging and add flash helper

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,16 +2,14 @@ class AccountActivationsController < ApplicationController
   include AccountActivationsHelper
   def edit
     user = User.find_by(email: params[:email])
-    puts "*"*1000
-    p user
-    p !user.activated?
-    p user.authenticated?(:activation, params[:id])
-    puts "*"*1000
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
       account_activation_success_flash_msg
       redirect_to welcome_path
+    elsif
+      user && user.activated? && user.authenticated?(:activation, params[:id])
+      account_activation_safety_flash_msg
     else
       account_activation_failure_flash_msg
       redirect_to root_url
--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -7,4 +7,8 @@ module AccountActivationsHelper
   def account_activation_failure_flash_msg
     flash[:danger] = 'Invalid activation link'
   end
+
+  def account_activation_safety_flash_msg
+    flash[:danger] = 'Please login'
+  end
 end
