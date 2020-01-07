cfea95d0ae0de681cf3db5d1a879d5a8e91c16ce
Chris Gilbert <chris.gilbert@songkick.com>
2020-01-07 12:44:50 +0000

    Update monzo link to Beetle's

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   before_action :logged_in_user, except: [:root, :contact]
   def rules_and_guidance
-    @payment_link = 'https://monzo.me/danielwilliams502/20.00?d=BTF%202020%20(type%20email%20address%20here)%20'.html_safe
+    @payment_link = 'https://monzo.me/alexanderwall2/20.00?d=BTF%202020%20(please%20insert%20email%20address%20here)%20'.html_safe
   end
 
   def blog
@@ -11,7 +11,7 @@ class StaticController < ApplicationController
   end
 
   def payment
-    @payment_link = 'https://monzo.me/danielwilliams502/20.00?d=BTF%202020%20(type%20email%20address%20here)%20'.html_safe
+    @payment_link = 'https://monzo.me/alexanderwall2/20.00?d=BTF%202020%20(please%20insert%20email%20address%20here)%20'.html_safe
   end
 
   def contact
