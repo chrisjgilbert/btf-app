b3f08586e1f26da5c05458b3d921efd2ca393759
Chris Gilbert <chrisgilbert4@icloud.com>
2020-02-07 11:25:25 +0000

    Add basic transfers limit validation

--- app/models/team_validator.rb
+++ app/models/team_validator.rb
@@ -1,5 +1,6 @@
 class TeamValidator < ActiveModel::Validator
   FAVOURITE_LIMIT = 5
+  TRANSFER_LIMIT  = 5
   NUMBER_OF_PICKS = Competition.all.count
   def validate(record)
     picks = record.picks.reject { |pick| pick.competitor_id == nil }
@@ -8,6 +9,9 @@ class TeamValidator < ActiveModel::Validator
     end
     if picks.select { |pick| pick.competitor.is_favourite? }.count > FAVOURITE_LIMIT
       record.errors[:base] << "Please review your team, you cannot have more than #{FAVOURITE_LIMIT} favourites"
+    end
+    if record.transfers_made > 4
+      record.errors[:base] << "Please review your team, you cannot make more than #{TRANSFER_LIMIT} transfers"
     end
     if record.captain_id == nil
       record.errors[:base] << "You need to select a captain"
